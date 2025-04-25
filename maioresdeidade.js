12633760d4019254bd6f3f766d20b712a8a60a16
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-04-25 10:53:22 -0300

    idades

---
+++ maioresdeidade.js
@@ -0,0 +1,9 @@
+let idades = ['10', '24', '15', '20'];
+let IdadesMaiores = [];
+
+for (let i = 0; i < idades.length; i++) {
+    if (idades[i]> 18) {
+        IdadesMaiores.push(idades[i]);
+        console.log(IdadesMaiores); 
+    }
+}
