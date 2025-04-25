da54ecc0e6b3d5f24471e07e271222132865ab9a
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-04-25 09:38:08 -0300

    playlist

---
+++ listademusica.js
@@ -0,0 +1,6 @@
+let playlist = ['shake it off', 'baby', 'dark horse', 'starboy', 'espresso'];
+let quantidade = playlist.length;
+
+for(let i = 0; i < quantidade; i++){
+    console.log(playlist[i]);
+}
