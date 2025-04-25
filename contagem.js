894525a093d1df1a06c3347d24d268ad02ada4ec
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-04-25 11:10:14 -0300

    frutas

---
+++ contagem.js
@@ -0,0 +1,11 @@
+let frutas = ['morango', 'uva', 'laranja', 'banana','goiaba', 'limão'];
+let NumeroTotal = frutas.length;
+
+console.log('Frutas')
+console.log(`O total de frutas é ${NumeroTotal}`);
+console.log('----------------------');
+
+for (let i = 0; i < NumeroTotal; i++){
+
+console.log(frutas[i]);
+}
